6b52bc352479668f6fad14d779837e50954ed4ba
liuchunxi <liuchunxi@hebao.im>
2019-02-21 15:40:26 +0800

    【新增】工程初始化

---
+++ AlgorithmLibrary/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AlgorithmLibrary
+//
+//  Created by liuchunxi on 2019/2/21.
+//  Copyright © 2019年 imera. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
