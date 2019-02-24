bdc9eb5cfd2e74eb3bad384da58246c71c64933e
liuchunxi <386418132@qq.com>
2019-02-24 21:55:21 +0800

    【新增】新增一道算法

--- AlgorithmLibrary/剑指offer/InterviewProblem5.cpp
+++ AlgorithmLibrary/剑指offer/InterviewProblem5.cpp
@@ -5,5 +5,57 @@
 //  Created by liuchunxi on 2019/2/23.
 //  Copyright © 2019年 imera. All rights reserved.
 //
+//  题意：
+//  输入一个链表的头结点，从尾到头反回来打印每个结点的值
 
 #include "InterviewProblem5.hpp"
+#include "stack"
+
+struct ListNode {
+    int value;
+    ListNode *pNext;
+};
+
+void AddToTail(ListNode **pHead, int value) {
+    ListNode *newNode = new ListNode();
+    newNode->value = value;
+    newNode->pNext = NULL;
+    
+    if (*pHead == NULL) {
+        *pHead = newNode;
+    }
+    else {
+        ListNode *pNode = *pHead;
+        
+        while (pNode->pNext != NULL) {
+            pNode = pNode->pNext;
+        }
+        
+        pNode->pNext = newNode;
+    }
+}
+
+void PrintListReverse(ListNode *headNode) {
+    std::stack<ListNode *>nodes;
+    
+    ListNode *pNode = headNode;
+    while (pNode != NULL) {
+        nodes.push(pNode);
+        pNode = pNode->pNext;
+    }
+    
+    while (!nodes.empty()) {
+        printf("%d", nodes.top()->value);
+        nodes.pop();
+    }
+}
+
+void PrintListReverse1(ListNode *headNode) {
+    if (headNode != NULL) {
+        if (headNode->pNext != NULL) {
+            PrintListReverse1(headNode->pNext);
+        }
+        
+        printf("%d", headNode->value);
+    }
+}
--- AlgorithmLibrary/剑指offer/InterviewProblem5.hpp
+++ AlgorithmLibrary/剑指offer/InterviewProblem5.hpp
@@ -5,6 +5,8 @@
 //  Created by liuchunxi on 2019/2/23.
 //  Copyright © 2019年 imera. All rights reserved.
 //
+//  题意：
+//  输入一个链表的头结点，从尾到头反回来打印每个结点的值
 
 #ifndef InterviewProblem5_hpp
 #define InterviewProblem5_hpp
