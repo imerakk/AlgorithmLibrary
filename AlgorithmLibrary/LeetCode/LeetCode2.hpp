9d58a28f9e29e6e3bdb75bb4d5e7de164d4b6be2
imera <386418132@qq.com>
2019-04-25 11:23:23 +0800

    【新增】新增一道算法

---
+++ AlgorithmLibrary/LeetCode/LeetCode2.hpp
@@ -0,0 +1,14 @@
+//
+//  LeetCode2.hpp
+//  AlgorithmLibrary
+//
+//  Created by liuchunxi on 2019/4/25.
+//  Copyright © 2019年 imera. All rights reserved.
+//
+
+#ifndef LeetCode2_hpp
+#define LeetCode2_hpp
+
+#include <stdio.h>
+
+#endif /* LeetCode2_hpp */
